d504b75c0372dc91a610740d945a6e3641351895
Nicholas Smith <nsmith@tenstorrent.com>
2026-06-17 11:42:35 -0500

    NOCOMMIT build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TRIPLE=riscv32-none-eabi
+
+cmake -S llvm -G Ninja -B build \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_INSTALL_PREFIX=/opt/riscv-toolchain \
+  -DCMAKE_C_COMPILER=clang \
+  -DCMAKE_CXX_COMPILER=clang++ \
+  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+  -DLLVM_ENABLE_PROJECTS="clang;lld" \
+  -DLLVM_TARGETS_TO_BUILD="X86;RISCV" \
+  -DLLVM_DEFAULT_TARGET_TRIPLE=${TRIPLE} \
+  -DLLVM_ENABLE_ASSERTIONS=ON \
+  -DLLVM_ENABLE_RUNTIMES="libc" \
+  -DLLVM_ENABLE_PER_TARGET_RUNTIME_DIR=OFF \
+  -DLLVM_LIBC_FULL_BUILD=ON \
+  -DLIBC_TARGET_TRIPLE=${TRIPLE} \
+  -DRUNTIMES_${TRIPLE}_LIBC_COMPILE_OPTIONS_DEFAULT="-mcpu=tt-tensix-brisc;-menable-experimental-extensions;-Wno-atomic-alignment" \
+  -DLLVM_RUNTIME_TARGETS=${TRIPLE}
+
+cmake --build build
+cmake --build build --target libc
+#cmake --install build
